d33c6eb584f76b277147629662256bb631c455ce
Fabio Claro <fabioclaroj@gmail.com>
2023-06-27 19:06:23 -0300

    Teste dos campos obrigatórios

--- cypress/integration/CAT-TAT.spec.js
+++ cypress/integration/CAT-TAT.spec.js
@@ -1,12 +1,26 @@
 /// <reference types="Cypress" />
 
-describe('Central de Atendimento ao Cliente TAT', function() {
-
-   it('verifica o título da aplicação', function() {
-    cy.visit('./src/index.html')
-
-    cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
-  
+describe('Central de Atendimento ao Cliente TAT', function () {
+   beforeEach(function () {
+      cy.visit('./src/index.html')
    })
+
+   it('verifica o título da aplicação', function () {
+      cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+
+   })
+
+   it('preenche os campos obrigatórios e envia o formulário', function () {
+      const longText = 'Teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste,teste, teste, teste, teste, teste, teste,teste, teste, teste, teste, teste, teste,teste, teste, teste, teste, teste, teste.'
+
+      cy.get('#firstName').type('Fabio')
+      cy.get('#lastName').type('Claro')
+      cy.get('#email').type('fabio@test.com')
+      cy.get('#open-text-area').type(longText, { delay: 0 })
+      cy.get('button[type="submit"]').click()
+
+      cy.get('.success').should('be.visible')
+   })
+
 })
-  
+
