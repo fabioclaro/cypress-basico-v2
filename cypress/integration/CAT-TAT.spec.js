809c1ea7e6ed34205e3e4b1b96c0231969673fcc
Fabio Claro <fabioclaroj@gmail.com>
2023-07-04 12:17:43 -0300

    Selecionar um produto (Mentoria) por seu valor (value)

--- cypress/integration/CAT-TAT.spec.js
+++ cypress/integration/CAT-TAT.spec.js
@@ -90,10 +90,16 @@ describe('Central de Atendimento ao Cliente TAT', function () {
          cy.get('.error').should('be.visible')
    })
 
-   it.only('seleciona um produto (YouTube) por seu texto', function () {
+   it('seleciona um produto (YouTube) por seu texto', function () {
       cy.get('#product')
         .select('YouTube')
         .should('have.value', 'youtube')
+   })
+
+   it('seleciona um produto (Mentoria) por seu valor (value)', function () {
+      cy.get('#product')
+        .select('mentoria')
+        .should('have.value', 'mentoria')
    })
 })
 
